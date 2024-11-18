86b46560ce32243c620a394c70183938843cc570
abedmatin <78045770+abedmatin@users.noreply.github.com>
2024-11-17 21:41:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -290,10 +290,10 @@ def main():
     cur_epochs = 0
     if opts.ckpt is not None and os.path.isfile(opts.ckpt):
         # https://github.com/VainF/DeepLabV3Plus-Pytorch/issues/8#issuecomment-605601402, @PytaichukBohdan
-        checkpoint = torch.load(opts.ckpt, map_location=torch.device('cpu'))
+        model.to(device)
+        checkpoint = torch.load(opts.ckpt, map_location=torch.device('cuda'))
         model.load_state_dict(checkpoint["model_state"])
         model = nn.DataParallel(model)
-        model.to(device)
         if opts.continue_training:
             optimizer.load_state_dict(checkpoint["optimizer_state"])
             scheduler.load_state_dict(checkpoint["scheduler_state"])
